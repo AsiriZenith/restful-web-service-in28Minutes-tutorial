2ee73c859c68c2e7bc2c406791110e529c130c27
AsiriZenith <asirisenith@gmail.com>
2021-09-28 23:11:16 +0530

    custome exception added

--- restful-web-service/src/main/java/com/spring/restfulwebservice/controller/UserResourse.java
+++ restful-web-service/src/main/java/com/spring/restfulwebservice/controller/UserResourse.java
@@ -1,14 +1,18 @@
 package com.spring.restfulwebservice.controller;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.spring.restfulwebservice.exception.UserNotFoundException;
 import com.spring.restfulwebservice.model.User;
 import com.spring.restfulwebservice.service.UserDaoService;
 
@@ -25,11 +29,24 @@ public class UserResourse {
 	
 	@GetMapping("/users/{id}")
 	public User retrieveUser(@PathVariable int id) {
-		return userDaoService.fingOne(id);
+		User user = userDaoService.fingOne(id);
+		
+		if (user == null) {
+			throw new UserNotFoundException("id-"+id);
+		}
+		
+		return user;
 	}
 	
 	@PostMapping("/users")
-	public void createUser(@RequestBody User user) {
-		userDaoService.Save(user);
+	public ResponseEntity<User> createUser(@RequestBody User user) {
+		User savedUser = userDaoService.Save(user);
+		
+		URI location = ServletUriComponentsBuilder
+							.fromCurrentRequest()
+							.path("/{id}")
+							.buildAndExpand(savedUser.getId()).toUri();
+		
+		return ResponseEntity.created(location).build();
 	}
 }
